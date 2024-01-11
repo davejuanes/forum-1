1697ac59c0c07f035d006f07f030a77358d84c43
David Mercado <dmercado@abc.gob.bo>
2024-01-11 12:23:17 -0400

    [ADD] UPGRADE VERSION OFFICE

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,24 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', \App\Livewire\ShowThreads::class)
-    ->middleware('auth')
-    ->name('dashboard');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
